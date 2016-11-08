ee2058a766e005b6ce93dbfca3e074f96d239ec4
Juan A Cortes <juan.cortes@gmail.com>
2016-11-07 19:31:35 -0500

    Inline greet method into a constructor.

--- inheritance.js
+++ inheritance.js
@@ -1,7 +1,7 @@
-// Inheritance
+// // Inheritance
+// //
+// // Given the following objects:
 //
-// Given the following objects:
-
 // var mom = {
 //   firstName: 'Alice',
 //   lastName: 'Wong',
@@ -13,7 +13,7 @@
 //   firstName: 'Ilene',
 //   hairColor: 'brown'
 // };
-//
+// //
 // // One
 // //
 // // Write code using __proto__ to make daughter inherit properties from mom. Print out each property that daughter has. What are they and what are their values?
@@ -45,53 +45,79 @@
 // // Three
 // //
 // // Given this Character type
+//
+// function Character () {
+//   this.health = 5;
+//   this.power = 1;
+//   this.speed = 5;
+// }
+//
+// Character.prototype.attack = function(enemy) {
+//   enemy.health -= this.power;
+// };
+//
+// Character.prototype.alive = function () {
+//   return this.health > 0;
+// };
+//
+// // Writing Goblin to use as enemy to attack
+// function Goblin() {
+//   this.name = "Goblin";
+//   this.health = 20;
+//   this.power = 2;
+// }
+//
+// // Write a Hero type that inherits it and overrides the attack method to randomly with 20% chance, double the dealt damage.
+//
+// function Hero() {
+//   // This line would allow Hero to inherit characteristics from the Character constructor
+//   Character.apply(this);
+//   this.name = "Hero";
+//   this.health = 10;
+//   this.power = 5;
+// }
+//
+// Hero.prototype = Object.create(Character.prototype);
+//
+// Hero.prototype.attack = function (enemy) {
+//   var chance = 20;
+//   var number = Math.floor((Math.random() * 100) + 1);
+//
+//   if (number <= chance) {
+//     enemy.health -= this.power * 2;
+//     console.log("Hero is causing double the damage");
+//   } else {
+//     enemy.health -= this.power;
+//     console.log("Hero is causing single damage");
+//   }
+// };
+//
+// var goblin = new Goblin();
+// var hero = new Hero();
+// hero.attack(goblin);
+//
+// Five
+//
+// Given this Person type:
+//
+// function Person(name) {
+//   this.name = name;
+// }
+//
+// Person.prototype.greet = function(other) {
+//   console.log('Hello ' + other.name + '! My name is ' + this.name + '.');
+// };
+//
+// Rewrite the above type by inlining it's greet method into it's constructor.
 
-function Character () {
-  this.health = 5;
-  this.power = 1;
-  this.speed = 5;
+function Person(name) {
+  this.name = name;
+  this.greet = function (other) {
+    console.log('Hello ' + other.name + '! My name is ' + this.name + '.');
+  };
 }
 
-Character.prototype.attack = function(enemy) {
-  enemy.health -= this.power;
-};
-
-Character.prototype.alive = function () {
-  return this.health > 0;
-};
-
-// Writing Goblin to use as enemy to attack
-function Goblin() {
-  this.name = "Goblin";
-  this.health = 20;
-  this.power = 2;
-}
-
-// Write a Hero type that inherits it and overrides the attack method to randomly with 20% chance, double the dealt damage.
-
-function Hero() {
-  // This line would allow Hero to inherit characteristics from the Character constructor
-  Character.apply(this);
-  this.name = "Hero";
-  this.health = 10;
-  this.power = 5;
-}
-
-Hero.prototype = Object.create(Character.prototype);
-
-Hero.prototype.attack = function (enemy) {
-  var chance = 20;
-  var number = Math.floor((Math.random() * 100) + 1);
-
-  if (number <= chance) {
-    enemy.health -= this.power * 2;
-    console.log("Hero is causing double the damage");
-  } else {
-    enemy.health -= this.power;
-    console.log("Hero is causing single damage");
-  }
-};
-
-var goblin = new Goblin();
-var hero = new Hero();
-hero.attack(goblin);
+var john = new Person('John');
+var mary = new Person('Mary');
+john.greet(mary);
+console.log(john);
